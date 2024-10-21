c4129ea822497e37df2fcafd23fad15b78e386ca
victoriapardinas <vpardinas@itba.edu.ar>
2024-10-21 19:06:15 -0300

    Agregar archivos para las consignas 16, 17 y 18

---
+++ 16_InfoMed_TP5_PardiñasMedinaSantinon.sql
@@ -0,0 +1,6 @@
+SELECT m.nombre AS medico, p.nombre AS paciente, COUNT(*) AS total_consultas 
+FROM consultas c
+JOIN medicos m ON c.id_medico = m.id_medico
+JOIN pacientes p ON c.id_paciente = p.id_paciente
+GROUP BY m.nombre, p.nombre  
+ORDER BY m.nombre, p.nombre;
---
+++ 18_InfoMed_TP5_PardiñasMedinaSantinon.sql
@@ -0,0 +1,7 @@
+SELECT 
+    m.nombre AS medico, 
+    COUNT(DISTINCT c.id_paciente) AS total_pacientes 
+FROM consultas c
+JOIN medicos m ON c.id_medico = m.id_medico
+GROUP BY m.nombre  
+ORDER BY total_pacientes DESC;  
